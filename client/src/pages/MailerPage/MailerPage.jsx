2c0a205e1b3f09298df18fd654de2f42061403a5
jeremy <webd0829@gmail.com>
2023-05-10 19:16:20 +0800

    set Mailer

--- client/src/pages/MailerPage/MailerPage.jsx
+++ client/src/pages/MailerPage/MailerPage.jsx
@@ -1,9 +1,15 @@
-import React from "react"
+import React from "react";
 
 const MailerPage = () => {
     return (
         <div className="mailer-page">
-            Mailer
+            <div className="tutorial-div">
+                <h5>React + Axios</h5>
+                <div className="tutorial-box">
+                    <p>code here</p>
+                </div>
+                <button>Get key</button>
+            </div>
         </div>
     );
 }
