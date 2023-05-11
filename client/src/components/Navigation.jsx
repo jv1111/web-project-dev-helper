c4744b67ed82bd127c93cbe8504f0267f4d50651
jeremy <webd0829@gmail.com>
2023-05-11 14:07:29 +0800

    Use link as ref "react-router-dom

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,15 +2,16 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import GOOGLE_SIGNIN_IMG from '../assets/img/google-signin.png';
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
     return (
         <>
             <Navbar bg="light" variant="light">
                 <Container>
-                    <Navbar.Brand href="/">Dev-Helper</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/">Dev-Helper</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="/Mailer">Mailer</Nav.Link>
+                        <Nav.Link as={Link} to="/Mailer">Mailer</Nav.Link>
                     </Nav>
                     <Nav>
                         <a href="#Login"><img src={GOOGLE_SIGNIN_IMG} alt="Login with google" /></a>
